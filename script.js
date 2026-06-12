49093d66d463bd8bf0af473a31a84f81ed677499
Maryammarwannnn <154735793+Maryammarwannnn@users.noreply.github.com>
2026-06-12 23:54:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const envelope = document.getElementById("envelope");
+const seal = document.getElementById("seal");
+
+const acceptBtn = document.getElementById("acceptBtn");
+
+const finalScreen =
+document.getElementById("finalScreen");
+
+let opened = false;
+
+envelope.addEventListener("click", () => {
+
+    if(opened) return;
+
+    opened = true;
+
+    seal.classList.add("break-seal");
+
+    setTimeout(() => {
+
+        envelope.classList.add("open");
+
+    },300);
+
+});
+
+acceptBtn.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+
+    envelope.style.display = "none";
+
+    finalScreen.style.display = "block";
+
+});
